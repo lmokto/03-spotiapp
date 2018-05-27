9a24810dc69285ab9d4b14987f76b9b6e2af116c
lmokto <lm.uranga@gmail.com>
2018-05-27 17:11:41 +0200

    spotify service search by artist

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
-  styles: []
+  templateUrl: './search.component.html'
 })
-export class SearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SearchComponent {
+  artistas: any[] = [];
+  constructor( private spotify: SpotifyService) { }
+  buscar( termino: string ) {
+    this.spotify.getArtista(termino).subscribe( ( response: any ) => {
+      console.log(response.artists.items);
+      this.artistas = response.artists.items;
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,16 +6,18 @@ export class SpotifyService {
 
   constructor( private http: HttpClient ) {
     console.log('Servicio listo para utilizar');
-   }
+  }
 
-   getNewReleases() {
+  headers = new HttpHeaders({
+    'Authorization': 'Bearer BQD8mM9JaCWdaFF0AAbwXsZE4Ym0RHUKJrsyBlBkxsddaQZdbHhWsM-2gyr_xcTf8hbAQVl1z5nGfjnnLDI'
+  });
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQD8mM9JaCWdaFF0AAbwXsZE4Ym0RHUKJrsyBlBkxsddaQZdbHhWsM-2gyr_xcTf8hbAQVl1z5nGfjnnLDI'
-    });
+  getNewReleases() {
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=5', { headers: this.headers });
+  }
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=5', { headers: headers });
-
+  getArtista(termino: string) {
+    return this.http.get(`https://api.spotify.com/v1/search/?q=${ termino }&type=artist&limit=5`, { headers: this.headers });
   }
 
 }
