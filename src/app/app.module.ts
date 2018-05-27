0cd1c45e8af576bd84931dddb9489ec35eaeaf9d
lmokto <lm.uranga@gmail.com>
2018-05-27 14:47:34 +0200

    add navbar in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// import rutas
+import { ROUTES } from './app.routes';
 
 
 @NgModule({
@@ -18,6 +21,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot( ROUTES, { useHash : true } ),
     BrowserModule
   ],
   providers: [],
