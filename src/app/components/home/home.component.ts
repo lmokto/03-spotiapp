9230864618dd8a2d00706d73aeeebb042395d2f5
lmokto <lm.uranga@gmail.com>
2018-05-27 15:04:35 +0200

    sample http request

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: []
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient  ) {
+    console.log('Constructor del Home hecho');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (response: any) => {
+      console.log(response);
+      this.paises = response;
+    });
+   }
 
   ngOnInit() {
   }
